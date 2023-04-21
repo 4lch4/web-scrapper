298bb265364a67b4dd99e575cea71267dfc4ff2b
zyzyx159 <dstellm@gmail.com>
2023-04-21 04:50:33 -0500

    array works, working on making object

--- mulitScrape.js
+++ mulitScrape.js
@@ -12,9 +12,26 @@ axios.get('https://www.theregister.com/2000/05/09/bofh_returns_from_the_dead/')
       return {
         title: $(element).find('div[class=header_right] > h1').text(),
         subtitle: $(element).find('div[class=header_right] > h2').text(),
-        story: $(element).find('div[id=body] > p').text(),
+        pubDate: $(element).find('span[class=dateline]').text(), // this picks up way too much junk. will need to process it into something useful
+        story: $(element).find('div[id=body] > p').toArray().map(element => $(element).text()),
       };
     }).get();
 
-console.log(JSON.stringify(episode));
+    let episode1 = new Map();
+
+//trying to add all the data to a map that I can call and use as an object
+
+    episode1.set('title', $(element).find('div[class=header_right] > h1').text());
+    episode1.set('subtitle', $(element).find('div[class=header_right] > h2').text());
+    episode1.set('pubDate', $(element).find('span[class=dateline]').text());
+//    episode1.set('')
+
+    const story = (episodeElements
+      .find('div[id=body] > p')
+      .toArray()
+      .map(element => $(element).text()));
+
+console.log(episode(0));
+
+//console.log(story);
 });
