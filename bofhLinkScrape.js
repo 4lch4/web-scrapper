e7cf8278e6d10f401f59bcd37fd592caf0149cb0
Daniel Stellmon <dstellm@gmail.com>
2023-06-17 03:50:17 -0500

    redownloaded bofhLinkScrape

---
+++ bofhLinkScrape.js
@@ -0,0 +1,34 @@
+import axios, * as others from 'axios';
+import * as cheerio from 'cheerio';
+import * as fs from 'fs';
+import * as df from 'date-format';
+
+let URL = process.argv[2];
+
+axios.get(URL).then(response => { // The HTML code of the website is stored in the "data" property of the response object
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const eplink = $('div[id=page] > div[id=main-col] > div[class=headlines]');
+    let sorw = $('srow rt-1*');
+
+var links = [];
+
+$('.story_link').each( function (){
+  var link = $(this).attr('href');
+    //  console.log(link);
+  links.push({link});
+});
+
+//console.log(links);
+
+fs.writeFile(
+    './OutPut/links.txt',
+    JSON.stringify(links),
+    function (err) {
+      if (err) {
+        console.log('stuff happened here')
+      }
+    }
+  )
+}) 
+
